a8a9d2ebc28115dd645cf44940979b42697a1ea9
zhangzhaoru <914413583@qq.com>
2021-06-09 11:35:17 +0800

    测试方法按字典序进行排序

--- src/test/java/com/thoughtworks/codepairing/service/ShoppingCartTest.java
+++ src/test/java/com/thoughtworks/codepairing/service/ShoppingCartTest.java
@@ -3,7 +3,9 @@ package com.thoughtworks.codepairing.service;
 import com.thoughtworks.codepairing.model.Customer;
 import com.thoughtworks.codepairing.model.Product;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,13 +13,8 @@ import java.util.List;
 import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ShoppingCartTest {
-
-    public static final int PRICE = 100;
-    public static final String PRODUCT = "Product";
-    private static ShoppingCart shoppingCart=null;
-
-
 
 //
     @Before
@@ -25,63 +22,75 @@ public class ShoppingCartTest {
         shoppingCart = ShoppingCart.getInstance();
     }
 
+    public static final int PRICE = 100;
+    public static final String PRODUCT = "Product";
+
+    private static ShoppingCart shoppingCart=null;
+
+
     @Test
     public void shouldCalculatePriceWithNoDiscount() {
-        Customer customer = new Customer("test1");
+        Customer customer = new Customer("test");
         shoppingCart.addCustomer(customer);
         List<Product> products = asList(new Product(PRICE, "", PRODUCT));
         shoppingCart.addProducts(customer, products);
         assertEquals(100.0, customer.getOrder().getTotalPrice(), 0.0);
+        shoppingCart.destroyAccount();
     }
 
     @Test
     public void shouldCalculateLoyaltyPointsWithNoDiscount() {
-        Customer customer = new Customer("test2");
+        Customer customer = new Customer("test");
         shoppingCart.addCustomer(customer);
         List<Product> products = asList(new Product(PRICE, "", PRODUCT));
         shoppingCart.addProducts(customer, products);
 
         assertEquals(20, customer.getOrder().getLoyaltyPoints());
+        shoppingCart.destroyAccount();
     }
 
     @Test
     public void shouldCalculatePriceFor10PercentDiscount() {
-        Customer customer = new Customer("test3");
+        Customer customer = new Customer("test");
         shoppingCart.addCustomer(customer);
         List<Product> products = asList(new Product(PRICE, "DIS_10_ABCD", PRODUCT));
         shoppingCart.addProducts(customer, products);
         assertEquals(90.0, customer.getOrder().getTotalPrice(), 0.0);
+        shoppingCart.destroyAccount();
     }
 
     @Test
     public void shouldCalculateLoyaltyPointsFor10PercentDiscount() {
-        Customer customer = new Customer("test4");
+        Customer customer = new Customer("test");
         shoppingCart.addCustomer(customer);
         List<Product> products = asList(new Product(PRICE, "DIS_10_ABCD", PRODUCT));
         shoppingCart.addProducts(customer, products);
         assertEquals(10, customer.getOrder().getLoyaltyPoints());
+        shoppingCart.destroyAccount();
     }
 
     @Test
     public void shouldCalculatePriceFor15PercentDiscount() {
-        Customer customer = new Customer("test5");
+        Customer customer = new Customer("test");
         shoppingCart.addCustomer(customer);
         List<Product> products = asList(new Product(PRICE, "DIS_15_ABCD", PRODUCT));
         shoppingCart.addProducts(customer, products);
         assertEquals(85.0, customer.getOrder().getTotalPrice(), 0.0);
+        shoppingCart.destroyAccount();
     }
 
     @Test
     public void shouldCalculateLoyaltyPointsFor15PercentDiscount() {
-        Customer customer = new Customer("test6");
+        Customer customer = new Customer("test");
         shoppingCart.addCustomer(customer);
         List<Product> products = asList(new Product(PRICE, "DIS_15_ABCD", PRODUCT));
         shoppingCart.addProducts(customer, products);
         assertEquals(6, customer.getOrder().getLoyaltyPoints());
+        shoppingCart.destroyAccount();
     }
 
     @Test
-    public void addMultiCustomerTest() {
+    public void addMultiCustomerTest() throws InterruptedException {
         Customer customer1 = new Customer("customer1");
         Customer customer2 = new Customer("customer2");
         Product product1 = new Product(120, "DIS_15_ABCD", "product1");
@@ -103,11 +112,10 @@ public class ShoppingCartTest {
         shoppingCart.show();
         System.out.println(customer1.getOrder().toString());
         System.out.println(customer2.getOrder().toString());
-
     }
 
     @Test
-    public void delProdectTest(){
+    public void delProdectTest() throws InterruptedException {
         Customer customer1 = shoppingCart.getCustomer("customer1");
         System.out.println(customer1.getOrder().toString());
         shoppingCart.delProdect(customer1,"product1");
